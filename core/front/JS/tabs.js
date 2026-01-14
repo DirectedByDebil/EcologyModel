447789fdd439b967f943251488c672e937d67f5c
SexKing <chromov-arsenij@mail.ru>
2026-01-14 23:58:07 +0300

    test(ui): started home page

---
+++ core/front/JS/tabs.js
@@ -0,0 +1,33 @@
+
+const tabs = {};
+const form = document.getElementById('form');
+
+const navTabs = document.getElementById('navTabs');
+const radios = navTabs.querySelectorAll('input[type=radio]');
+
+radios.forEach(radioBtn => {
+    
+    const option = radioBtn.id;
+    const groupName = radioBtn.name;
+    const isChecked = radioBtn.checked;
+
+    const tab = form.querySelector(`#${option}`);
+    
+    tabs[groupName] ??= {
+        elements: [],
+        current: undefined
+    };
+    tabs[groupName].elements.push(tab);
+
+    if (isChecked) {
+        tabs[groupName].current = tab;
+    }
+
+    radioBtn.addEventListener('click', (e)=>{
+
+        tabs[groupName].current?.removeAttribute('checked');
+        
+        tabs[groupName].current = tab;
+        tabs[groupName].current?.setAttribute('checked', true);
+    });
+});
